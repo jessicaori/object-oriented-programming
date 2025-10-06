68a5406cc6f28252be35b0ba470b13849aefbfac
Jessica Orihuela Rojas <66490864+jessicaori@users.noreply.github.com>
2025-10-06 16:53:40 -0400

    Update OutputFormatterFactoryTests.cs

--- ObjectOrientedDesign/TestDocCli.Tests/AppCore/OutputFormatterFactoryTests.cs
+++ ObjectOrientedDesign/TestDocCli.Tests/AppCore/OutputFormatterFactoryTests.cs
@@ -1,34 +1,91 @@
 using TestDocCli.AppCore;
 using TestDocCli.Errors;
 using TestDocCli.Formatters;
+using Xunit;
+using System;
+using System.Collections.Generic;
 
 namespace TestDocCli.Tests.AppCore;
 
 public class OutputFormatterFactoryTests
 {
-  [Theory]
-  [InlineData("", typeof(PlainConsoleFormatter))]
-  [InlineData("console", typeof(PlainConsoleFormatter))]
-  [InlineData("text", typeof(PlainConsoleFormatter))]
-  [InlineData("html", typeof(HtmlFormatter))]
-  [InlineData("md", typeof(MarkdownFormatter))]
-  [InlineData("markdown", typeof(MarkdownFormatter))]
-  public void Create_KnownHtmlFormat_ReturnsHtmlFormatter(string input, Type expectedType)
-  {
-    var factory = new OutputFormatterFactory();
+    public static IEnumerable<object[]> SupportedFormats => new List<object[]>
+    {
+        new object[] { "", typeof(PlainConsoleFormatter) },
+        new object[] { "console", typeof(PlainConsoleFormatter) },
+        new object[] { "text", typeof(PlainConsoleFormatter) },
+        new object[] { "html", typeof(HtmlFormatter) },
+        new object[] { "md", typeof(MarkdownFormatter) },
+        new object[] { "markdown", typeof(MarkdownFormatter) },
+    };
 
-    IOutputFormatter formatter = factory.Create(input);
+    [Theory]
+    [MemberData(nameof(SupportedFormats))]
+    public void Create_SupportedFormats_ReturnsCorrectFormatter(string input, Type expectedType)
+    {
+        var factory = new OutputFormatterFactory();
 
-    Assert.IsType(expectedType, formatter);
-  }
+        IOutputFormatter formatter = factory.Create(input);
 
-  [Fact]
-  public void Create_UnknownFormat_ThrowsConfigurationException()
-  {
-    var factory = new OutputFormatterFactory();
+        Assert.IsType(expectedType, formatter);
+    }
 
-    Assert.Throws<ConfigurationException>(() => factory.Create("pdf"));
-  }
+    [Fact]
+    public void Create_UnknownFormat_ThrowsConfigurationException()
+    {
+        var factory = new OutputFormatterFactory();
 
-  // TODO: Add more tests
+        Assert.Throws<ConfigurationException>(() => factory.Create("pdf"));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    public void Create_NullOrWhitespaceInput_ReturnsPlainConsoleFormatter(string input)
+    {
+        var factory = new OutputFormatterFactory();
+
+        var formatter = factory.Create(input);
+
+        Assert.IsType<PlainConsoleFormatter>(formatter);
+    }
+
+    [Theory]
+    [InlineData("HTML", typeof(HtmlFormatter))]
+    [InlineData("Md", typeof(MarkdownFormatter))]
+    [InlineData("MARKDOWN", typeof(MarkdownFormatter))]
+    [InlineData("TEXT", typeof(PlainConsoleFormatter))]
+    public void Create_FormatInput_IsCaseInsensitive(string input, Type expectedType)
+    {
+        var factory = new OutputFormatterFactory();
+
+        var formatter = factory.Create(input);
+
+        Assert.IsType(expectedType, formatter);
+    }
+
+    [Theory]
+    [InlineData(" html ", typeof(HtmlFormatter))]
+    [InlineData("  markdown", typeof(MarkdownFormatter))]
+    [InlineData("text  ", typeof(PlainConsoleFormatter))]
+    public void Create_InputWithExtraWhitespace_ReturnsCorrectFormatter(string input, Type expectedType)
+    {
+        var factory = new OutputFormatterFactory();
+
+        var formatter = factory.Create(input);
+
+        Assert.IsType(expectedType, formatter);
+    }
+
+    [Fact]
+    public void Create_CalledMultipleTimes_ReturnsNewInstances()
+    {
+        var factory = new OutputFormatterFactory();
+
+        var first = factory.Create("html");
+        var second = factory.Create("html");
+
+        Assert.NotSame(first, second);
+    }
 }
